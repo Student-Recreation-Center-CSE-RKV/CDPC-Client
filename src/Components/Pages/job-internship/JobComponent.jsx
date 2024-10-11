d1d4c8c33544009813dffa05064b4371abb8485c
cherrycharan238 <cherryiiit1234@gmail.com>
2024-10-11 12:07:32 +0530

    job List categirised

--- src/Components/Pages/job-internship/JobComponent.jsx
+++ src/Components/Pages/job-internship/JobComponent.jsx
@@ -4,15 +4,15 @@ import JobCard from './JobCard';
 import jobs from './JobsList';
 
 const JobList = (props) => {
-  const {bgColor} = props;
+  const {bgColor , jobType} = props;
   return <div  style={{padding:"20px", color:"black",marginTop:props.mt || "50px", backgroundColor:bgColor}}>
     <h1 style={{
       textAlign:"center"
     }} >{props.heading || "Job Postings"}</h1>
     <Grid style={{display:"flex" , flexDirection:"row" , marginTop : "50px"
       }} className='job-list' container spacing={2} justifyContent="center">
-      {jobs.map((job, index) => (
-          <JobCard job={job} />
+      {jobs[jobType].map((job, index) => (
+          <JobCard key={job.id} job={job} />
       ))}
     </Grid>
   </div>
