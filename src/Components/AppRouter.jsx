a8cd41d0cdd004d9cfccb87d4c56e420f2a0bc17
Bhargavi_Seelam <bhargaviseelam2006@gmail.com>
2024-11-01 22:46:45 +0530

    Designed the interview preparation page in  Placement Preparation

--- src/Components/AppRouter.jsx
+++ src/Components/AppRouter.jsx
@@ -12,6 +12,7 @@ import AlumniNetworks from './Pages/AlumniNetworks';
 import Registration from './Pages/Registration';
 import CodingPracticePage from './Pages/CodingPracticePage';
 import ResumeBuildingPage from './Pages/ResumeBuildingPage';
+import InterviewPreparation from './Pages/InterviewPreparation';
 const AppRouter = () => {
   return (
     <Router>
@@ -30,6 +31,7 @@ const AppRouter = () => {
           <Route path='alumni-networks' element={<AlumniNetworks/>}/>
           <Route path='codingpracticepage' element={<CodingPracticePage/>}/>
           <Route path="resumebuildingpage" element={<ResumeBuildingPage/>}/>
+          <Route path="interviewpreparation" element={<InterviewPreparation/>}/>
         </Route>
       </Routes>
     </Router>
